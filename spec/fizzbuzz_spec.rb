ac3286d6d66d1f0273364ba489d1c8cdc85521a3
Ben Stein <bumstain@gmail.com>
2016-04-13 00:12:41 +1000

    All tests passing

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,20 +4,20 @@ describe "Fixnum" do
   describe "fizzbuzz" do
 
       it 'returns "Fizz" when passed 3' do
-        expect(3.fizzbuzz).to eq "Fizz"
+        expect(3.fizzbuzz(3)).to eq "Fizz"
       end
 
       it 'returns "Buzz" when passed 5' do
-        expect(5.fizzbuzz).to eq "Buzz"
+        expect(5.fizzbuzz(5)).to eq "Buzz"
       end
     
 
       it 'returns "FizzBuzz" when passed 15' do
-        expect(15.fizzbuzz).to eq "Fizzbuzz"
+        expect(15.fizzbuzz(15)).to eq "Fizzbuzz"
       end
 
       it 'returns other number' do
-        expect(7.fizzbuzz).to eq 7
+        expect(8.fizzbuzz(8)).to eq 8
       end
     
   end
